765a8aba208729ce1231513b6d99c3d9fa888753
nguyenhuong791123 <nguyenhuong791123@gmail.com>
2019-05-12 22:28:17 +0900

    Test Flow

--- dags/webhtml.py
+++ dags/webhtml.py
@@ -1,1 +1,22 @@
-#!/usr/bin/bash
+from airflow import DAG
+from airflow.operators import BashOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'vagrant',
+    'start_date': datetime(2016, 6, 12, 15, 00, 00)
+}
+
+dag = DAG('airflow_logs', default_args=default_args, schedule_interval='0/1 * * * *')
+
+task1 = BashOperator(
+    task_id='print_date',
+    bash_command='date',
+    dag=dag)
+
+task2 = BashOperator(
+    task_id='print_hello-world',
+    bash_command='echo "hello world!!"',
+    dag=dag)
+
+task1.set_downstream(task2)
