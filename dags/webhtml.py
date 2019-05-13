e1e6a50a997b83e3e92efbb57429d1a7bd2a15cf
nguyenhuong791123 <nguyenhuong791123@gmail.com>
2019-05-13 23:37:01 +0900

    Change Add Task

--- dags/webhtml.py
+++ dags/webhtml.py
@@ -1,10 +1,15 @@
 from airflow import DAG
-from airflow.operators import BashOperator
+#Please import from 'airflow.operators.[operator_module]' instead. Support for direct imports will be dropped entirely in Airflow 2.0.
+from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
+from airflow.operators.email_operator import EmailOperator
 
 default_args = {
-    'owner': 'vagrant',
-    'start_date': datetime(2016, 6, 12, 15, 00, 00)
+    'owner': 'Airflow'
+    ,'start_date': datetime(2016, 6, 12, 15, 00, 00)
+    ,'email': ['nguyenhuong791123@gmail.com']
+    ,'email_on_failure': False
+    ,'email_on_retry': False,
 }
 
 dag = DAG('airflow_logs', default_args=default_args, schedule_interval='0/1 * * * *')
@@ -19,4 +24,22 @@ task2 = BashOperator(
     bash_command='echo "hello world!!"',
     dag=dag)
 
+
+EMAIL_CONTENT = """
+
+<ul>
+    <li>Instatnce key: %s</li>
+    <li>Owner: %s</li>
+    <li>Host: %s</li>
+</ul>
+
+""" % ("{{ task_instance_key_str }}", "{{ task.owner}}", "{{ ti.hostname }}",)
+send_mail = EmailOperator (
+    dag=dag,
+    task_id="send_mail",
+    to=["nguyenhuong791123@gmail.com"],
+    subject="バッチ成功: 実行日 {{ ds }}",
+    html_content=EMAIL_CONTENT)
+
 task1.set_downstream(task2)
+send_mail.set_upstream(task2)
